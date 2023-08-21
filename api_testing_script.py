fbc6fcc3e0dd57830d5a5482d1a10ea1b836e1d8
mayur <mayur.1992@yahoo.com>
2023-08-21 22:06:22 +0100

    added requirements.txt and first api fetch

--- api_testing_script.py
+++ api_testing_script.py
@@ -4,7 +4,7 @@ import json
 f =  open("data_to_get_started.json") 
 json_data = json.load(f)
 
-premier_league_id= json_data["league_ids"]["premier_league"])
+premier_league_id= json_data["league_ids"]["premier_league"]
 
 f = open("credentials.json") 
 json_data_creds = json.load(f)
@@ -12,17 +12,27 @@ json_data_creds = json.load(f)
 rapid_api_key = json_data_creds["api_keys"]["rapidapi_api_key"]
 
 
+print("starting api code")
 
 #get all team ids from premier league
-url
+url = "https://api-football-v1.p.rapidapi.com/v3/teams"
 headers = {
 	"X-RapidAPI-Key": rapid_api_key,
 	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 }
 
 params = {
-	"league" = str(premier_league_id)
+	"league": premier_league_id,
+	"season":2023
 }
 
-response = requests.get(url, headers=headers, params=params)
+response = requests.get(url = url, headers=headers, params=params)
+print(response)
+print(response.text)
+json_data = response.json()
+
+with open("team_data.json", "w") as f:
+    json.dump(json_data, f)
+
+print("completed api code")
 
